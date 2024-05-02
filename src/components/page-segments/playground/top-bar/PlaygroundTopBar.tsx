50e020caa89585f64bfb209d856d0061f58af12f
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-01 21:28:53 -0400

    asdf

--- src/components/page-segments/playground/top-bar/PlaygroundTopBar.tsx
+++ src/components/page-segments/playground/top-bar/PlaygroundTopBar.tsx
@@ -1,13 +1,16 @@
 import { EmptyObject } from '../../../../types/empty';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FunctionComponent } from 'react';
 import { INTERNAL_LINKS } from '../../../../helpers/urls';
 import Link from '../../../shared/elements/Link';
 import PulsingOrb from '../../../shared/animated/PulsingOrb';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
 
 const PlaygroundTopBar: FunctionComponent<EmptyObject> = () => {
   return (
-    <div className="w-screen h-16 border-b border-b-stone-600 border-dashed">
+    <div className="flex justify-between content-between w-screen h-16 border-b border-b-stone-600 border-dashed">
       <LinkHomeBlock />
+      <ProviderKeysBlock />
     </div>
   );
 };
@@ -20,6 +23,16 @@ const LinkHomeBlock = () => {
           <PulsingOrb />
         </div>
       </Link>
+    </div>
+  );
+};
+
+const ProviderKeysBlock = () => {
+  return (
+    <div className="h-full w-36 flex items-center justify-center border-l border-l-stone-600 border-dashed">
+      <div className="w-full h-full flex items-center justify-center hover:bg-blue-950 cursor-pointer">
+        <FontAwesomeIcon icon={faKey} style={{ color: 'white', width: '20px', height: '20px' }} />
+      </div>
     </div>
   );
 };
