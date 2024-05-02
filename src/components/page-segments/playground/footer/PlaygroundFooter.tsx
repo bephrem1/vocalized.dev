67e86e3bfd2e931441aaea54b47386f0de9c3335
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-02 15:35:55 -0400

    Update PlaygroundFooter.tsx

--- src/components/page-segments/playground/footer/PlaygroundFooter.tsx
+++ src/components/page-segments/playground/footer/PlaygroundFooter.tsx
@@ -6,7 +6,7 @@ import XLogo from '../../../../icons/lib/companies/XLogo';
 
 const PlaygroundFooter: FunctionComponent<EmptyObject> = () => {
   return (
-    <div className="flex justify-between content-between w-screen h-8 border-t border-t-stone-600 border-dashed">
+    <div className="flex justify-between content-between w-screen h-10 border-t border-t-stone-600 border-dashed">
       <ConnectionStatusBlock />
       <FollowOnX />
     </div>
@@ -30,9 +30,9 @@ const FollowOnX = () => {
       <Link type="external" dest={EXTERNAL_LINKS.BEN.TWITTER} openInNewWindow>
         <div className="w-full h-full flex items-center justify-center hover:bg-neutral-900">
           <div className="flex flex-row items-center justify-center">
-            <span className="mr-1.5">
-              <p className="text-white text-xs inline">follow </p>
-              <p className="text-white text-xs inline">@notbenyam</p>
+            <span className="mr-1.5 flex items-center">
+              <p className="text-white text-xs mr-1 inline">follow</p>
+              <p className="text-stone-300 text-xs inline">@notbenyam</p>
             </span>
             <XLogo className="w-3 h-3" stroke="fill-white" />
           </div>
