fbae8d9a37daa66ce16a0f93fa9111abb8424a5d
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-01 20:15:05 -0400

    Update PlaygroundTopBar.tsx

--- src/components/page-segments/playground/top-bar/PlaygroundTopBar.tsx
+++ src/components/page-segments/playground/top-bar/PlaygroundTopBar.tsx
@@ -7,20 +7,20 @@ import PulsingOrb from '../../../shared/animated/PulsingOrb';
 const PlaygroundTopBar: FunctionComponent<EmptyObject> = () => {
   return (
     <div className="w-screen h-16 border-b border-b-stone-600 border-dashed">
-      <div className="h-full w-24 flex items-center justify-center border-r border-r-stone-600 border-dashed">
-        <LinkHomeBlock />
-      </div>
+      <LinkHomeBlock />
     </div>
   );
 };
 
 const LinkHomeBlock = () => {
   return (
-    <Link type="internal" dest={INTERNAL_LINKS.HOME} openInNewWindow>
-      <div className="w-full h-full flex items-center justify-center hover:bg-emerald-950">
-        <PulsingOrb />
-      </div>
-    </Link>
+    <div className="h-full w-24 flex items-center justify-center border-r border-r-stone-600 border-dashed">
+      <Link type="internal" dest={INTERNAL_LINKS.HOME} openInNewWindow>
+        <div className="w-full h-full flex items-center justify-center hover:bg-emerald-950">
+          <PulsingOrb />
+        </div>
+      </Link>
+    </div>
   );
 };
 
