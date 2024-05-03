fa721696b59c3136a154fd4f6c8fa7144f793fae
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-03 02:37:55 -0400

    Create CredentialCard.tsx

---
+++ src/components/page-segments/playground/credentials/cards/CredentialCard.tsx
@@ -0,0 +1,8 @@
+import { EmptyObject } from '../../../../../types/empty';
+import { FunctionComponent } from 'react';
+
+const CredentialCard: FunctionComponent<EmptyObject> = () => {
+  return null;
+};
+
+export default CredentialCard;
