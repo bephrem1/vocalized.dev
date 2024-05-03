1a9a1bd23cf5508c83a6fb96f71cba921e176667
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-03 16:50:10 -0400

    modal start

--- src/components/page-segments/playground/credentials/cards/lib/VapiCredentialCard.tsx
+++ src/components/page-segments/playground/credentials/cards/lib/VapiCredentialCard.tsx
@@ -1,9 +1,22 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '../../../../../shared/shadcn/components/ui/dialog';
 import { FunctionComponent, useContext } from 'react';
 
+import { Button } from '../../../../../shared/shadcn/components/ui/button';
 import CredentialCard from '../CredentialCard';
 import { CredentialsContext } from '../../../../../../context/credentials';
 import { EmptyObject } from '../../../../../../types/empty';
+import { Input } from '../../../../../shared/shadcn/components/ui/input';
+import { Label } from '../../../../../shared/shadcn/components/ui/label';
 import { ProviderId } from '../../../../../../fixtures/providers';
+import clsx from 'clsx';
 
 const VapiCredentialCard: FunctionComponent<EmptyObject> = () => {
   const { checkCredentialsSet } = useContext(CredentialsContext);
@@ -11,11 +24,28 @@ const VapiCredentialCard: FunctionComponent<EmptyObject> = () => {
   const credentialsSet = checkCredentialsSet({ providerId: ProviderId.Vapi });
 
   return (
-    <CredentialCard
-      providerName="Vapi"
-      providerImageUrl="/images/logos/vapi.png"
-      credentialsSet={credentialsSet}
-    />
+    <AddCredentialsModalWrapper>
+      <div>
+        <CredentialCard
+          providerName="Vapi"
+          providerImageUrl="/images/logos/vapi.png"
+          credentialsSet={credentialsSet}
+        />
+      </div>
+    </AddCredentialsModalWrapper>
+  );
+};
+
+const AddCredentialsModalWrapper = ({ children }) => {
+  return (
+    <Dialog open={true}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="bg-neutral-950 border border-neutral-500">
+        <div className="w-full h-full">
+          <img src="/images/logos/vapi.png" className="w-16 h-16 rounded-md" draggable={false} />
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
