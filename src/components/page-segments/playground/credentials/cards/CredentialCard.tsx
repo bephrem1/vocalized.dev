99a58b78129cf3158b6cdc627e52f6e941aba8a9
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-03 11:24:19 -0400

    credentials

--- src/components/page-segments/playground/credentials/cards/CredentialCard.tsx
+++ src/components/page-segments/playground/credentials/cards/CredentialCard.tsx
@@ -1,8 +1,67 @@
-import { EmptyObject } from '../../../../../types/empty';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger
+} from '../../../../shared/shadcn/components/ui/tooltip';
 
-const CredentialCard: FunctionComponent<EmptyObject> = () => {
-  return null;
+import clsx from 'clsx';
+
+interface CredentialCardProps {
+  providerName: string;
+  providerImageUrl: string;
+  credentialsSet?: boolean;
+}
+
+const CredentialCard: FunctionComponent<CredentialCardProps> = ({
+  providerName,
+  providerImageUrl,
+  credentialsSet = false
+}) => {
+  const [hovered, setHovered] = useState<boolean>(false);
+
+  const rootClass = clsx([
+    'flex flex-row py-6 px-8',
+    'border',
+    credentialsSet ? 'border-stone-800' : 'border-stone-700',
+    credentialsSet ? 'border-solid' : 'border-dashed',
+    credentialsSet ? 'hover:bg-red-950' : 'hover:bg-neutral-900',
+    'rounded-lg transition ease-in-out duration-300 cursor-pointer'
+  ]);
+  const imageClass = clsx([
+    'w-12 h-12 mb-3.5',
+    hovered ? 'rounded-3xl' : 'rounded-lg',
+    'transition-all duration-300 ease-in-out'
+  ]);
+  const tooltipContentClass = clsx([
+    'm-2',
+    credentialsSet ? 'bg-red-950' : 'bg-neutral-800',
+    'transition-all duration-300 ease-in-out'
+  ]);
+
+  const tooltipLabel = credentialsSet
+    ? `Clear ${providerName} Credentials`
+    : `Set ${providerName} Credentials`;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <div
+          className={rootClass}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <div className="flex flex-col items-center">
+            <img src={providerImageUrl} className={imageClass} draggable={false} />
+            <p className="text-white text-md font-medium">{providerName}</p>
+          </div>
+        </div>
+      </TooltipTrigger>
+      <TooltipContent className={tooltipContentClass}>
+        <p className="text-white">{tooltipLabel}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
 };
 
 export default CredentialCard;
--- src/components/page-segments/playground/credentials/cards/lib/VapiCredentialCard.tsx
+++ src/components/page-segments/playground/credentials/cards/lib/VapiCredentialCard.tsx
@@ -1,40 +1,14 @@
-import { FunctionComponent, useState } from 'react';
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '../../../../../shared/shadcn/components/ui/tooltip';
-
+import CredentialCard from '../CredentialCard';
 import { EmptyObject } from '../../../../../../types/empty';
-import clsx from 'clsx';
+import { FunctionComponent } from 'react';
 
 const VapiCredentialCard: FunctionComponent<EmptyObject> = () => {
-  const [hovered, setHovered] = useState<boolean>(false);
-
-  const imageClass = clsx([
-    'w-12 h-12 mb-3.5',
-    hovered ? 'rounded-3xl' : 'rounded-lg',
-    'transition-all duration-300 ease-in-out'
-  ]);
-
   return (
-    <Tooltip>
-      <TooltipTrigger>
-        <div
-          className="flex flex-row py-6 px-8 border border-stone-600 border-dashed hover:bg-neutral-900 rounded-lg transition ease-in-out duration-300 cursor-pointer"
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
-          <div className="flex flex-col items-center">
-            <img src="/images/logos/vapi.png" className={imageClass} draggable={false} />
-            <p className="text-white text-md font-medium">Vapi</p>
-          </div>
-        </div>
-      </TooltipTrigger>
-      <TooltipContent className="bg-neutral-800 m-2">
-        <p className="text-white">Set Vapi Credentials</p>
-      </TooltipContent>
-    </Tooltip>
+    <CredentialCard
+      providerName="Vapi"
+      providerImageUrl="/images/logos/vapi.png"
+      credentialsSet={true}
+    />
   );
 };
 
