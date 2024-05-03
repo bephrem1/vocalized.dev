99ccf4527df2dfe7b98168a51b4c7c0795f710bf
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-03 02:17:18 -0400

    Update VapiCredentialCard.tsx

--- src/components/page-segments/playground/credentials/cards/lib/VapiCredentialCard.tsx
+++ src/components/page-segments/playground/credentials/cards/lib/VapiCredentialCard.tsx
@@ -1,4 +1,9 @@
 import { FunctionComponent, useState } from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger
+} from '../../../../../shared/shadcn/components/ui/tooltip';
 
 import { EmptyObject } from '../../../../../../types/empty';
 import clsx from 'clsx';
@@ -7,19 +12,29 @@ const VapiCredentialCard: FunctionComponent<EmptyObject> = () => {
   const [hovered, setHovered] = useState<boolean>(false);
 
   const imageClass = clsx([
-    'w-12 h-12',
+    'w-12 h-12 mb-2',
     hovered ? 'rounded-lg' : 'rounded-3xl',
     'transition-all duration-300 ease-in-out'
   ]);
 
   return (
-    <div
-      className="flex flex-row p-4 border border-stone-600 border-dashed hover:bg-neutral-900 rounded-lg transition ease-in-out duration-300 cursor-pointer"
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <img src="/images/logos/vapi.png" className={imageClass} draggable={false} />
-    </div>
+    <Tooltip>
+      <TooltipTrigger>
+        <div
+          className="flex flex-row py-6 px-8 border border-stone-600 border-dashed hover:bg-neutral-900 rounded-lg transition ease-in-out duration-300 cursor-pointer"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <div className="flex flex-col items-center">
+            <img src="/images/logos/vapi.png" className={imageClass} draggable={false} />
+            <p className="text-white text-lg font-medium">Vapi</p>
+          </div>
+        </div>
+      </TooltipTrigger>
+      <TooltipContent className="bg-neutral-800 m-2">
+        <p className="text-white">Set Your Vapi Credentials</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
 
