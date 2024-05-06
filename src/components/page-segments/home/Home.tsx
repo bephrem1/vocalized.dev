340c8d16618207e19c2963a00d697fd3dfcaf75c
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-06 14:11:43 -0400

    waveform

--- src/components/page-segments/home/Home.tsx
+++ src/components/page-segments/home/Home.tsx
@@ -1,15 +1,13 @@
 import { EmptyObject } from '../../../types/empty';
 import { FunctionComponent } from 'react';
-import { INTERNAL_LINKS } from '../../../helpers/urls';
-import Link from '../../shared/elements/Link';
+import HomepageWaveform from './components/Waveform';
+import React from 'react';
 
 const Home: FunctionComponent<EmptyObject> = () => {
   return (
     <div className="w-screen h-screen">
       <div className="flex flex-row w-full h-full items-center justify-center">
-        <Link type="internal" dest={INTERNAL_LINKS.PLAYGROUND}>
-          <p className="text-white underline">playground</p>
-        </Link>
+        <HomepageWaveform />
       </div>
     </div>
   );
