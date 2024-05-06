f2005b02bf2bcaa2b9a4f1c0cceb3e6ee093bdbe
Benyam Ephrem <ephremdsg@gmail.com>
2024-05-06 15:15:01 -0400

    Update Home.tsx

--- src/components/page-segments/home/Home.tsx
+++ src/components/page-segments/home/Home.tsx
@@ -9,15 +9,15 @@ import React from 'react';
 const Home: FunctionComponent<EmptyObject> = () => {
   return (
     <div className="w-screen h-screen">
-      <div className="w-full h-full z-50">
+      <div className="relative w-full h-full z-50">
         <div className="flex flex-row w-full h-full items-center justify-center">
           <Link type="internal" dest={INTERNAL_LINKS.PLAYGROUND} fillContainer={false}>
             <Button className="border-dashed border-neutral-800">{'<Playground />'}</Button>
           </Link>
         </div>
       </div>
-      <div className="absolute w-screen h-screen z-[-1] top-0 left-0">
-        <HomepageWaveform fadeInDelayMs={1000} />
+      <div className="absolute w-screen h-screen z-10 top-0 left-0">
+        <HomepageWaveform fadeInDelayMs={500} />
       </div>
     </div>
   );
